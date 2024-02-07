d0a922e8f41e0af59a8b2913cf6223288c08abf9
Derrick Wilde <derrick.wilde@gmail.com>
2024-02-07 12:58:31 -0600

    Initial Commit

---
+++ ChartsDemo/ChartsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsDemoApp.swift
+//  ChartsDemo
+//
+//  Created by Derrick Wilde on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ChartsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
